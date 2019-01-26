29a1149777d3f8f828915e88faa273bc7adf61e0
Hudhaifa-PC\Hudhaifa <info@huthaifa-abd.com>
2019-01-26 20:12:29 +0100

    [Code / Art] Reach Home

--- Assets/dreimeilenstiefel/Scripts/Board and Grid/House.cs
+++ Assets/dreimeilenstiefel/Scripts/Board and Grid/House.cs
@@ -6,30 +6,43 @@ public class House : MonoBehaviour
 {
     //House Prefab
     public GameObject housePrefab;
-
+    public float stepSize = 0.7680001f;
+    private bool isSpawnned = false;
+    private Vector3 houseLocation;
     // Start is called before the first frame update
     void Start()
     {
-        SpawnHouse();
+        isSpawnned = SpawnHouse();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if(isSpawnned)
+       Debug.Log(Vector3.Distance(houseLocation, GameObject.FindGameObjectWithTag("Player").transform.position));
+        if (Vector3.Distance(houseLocation, GameObject.FindGameObjectWithTag("Player").transform.position) < 0.68f)
+        {
+            Debug.Log("Game Over");
+            GUIManager.instance.GameOver();
+        }
     }
 
-    private void SpawnHouse()
+    private bool SpawnHouse()
     {
-        float hstep = 0;
-        float vstep = 0;
-
+        //House location
+        Vector2 location;
+        //Create a new house game object
         GameObject houseHandler = Instantiate(housePrefab) as GameObject;
+        //Reset Game Object Transform
         houseHandler.transform.parent = this.transform;
-        hstep = 0.2f + Random.Range(1, 7);
-        vstep = 0.2f + Random.Range(1, 7);
-        if (Random.Range(1, 2) == 1) { hstep = 16.03f; } else { vstep = 8.30f; }
-        houseHandler.transform.localPosition = new Vector3(hstep, vstep, 0);
+        //Generate Random Loaction
+        location.x = stepSize * Random.Range(1, 7);
+        location.y = stepSize * Random.Range(1, 7);
+        Debug.Log(location.x + " - " + location.y);
+        //Push to the board edge 
+        if (location.y > location.x) { location.x = 16.03f; } else { location.y = 7.56f; }
+        houseHandler.transform.localPosition = new Vector3(location.x, location.y, 0);
+        houseLocation = houseHandler.transform.position;
+        return true;
     }
-
 }
