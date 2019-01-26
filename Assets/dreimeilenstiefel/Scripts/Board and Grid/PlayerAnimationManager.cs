db5b8dddefc20388d3dac9cf7bcf1603df7995c0
Hudhaifa-PC\Hudhaifa <info@huthaifa-abd.com>
2019-01-26 17:18:55 +0100

    [Level Design] Player Sprite / Basic Animation

---
+++ Assets/dreimeilenstiefel/Scripts/Board and Grid/PlayerAnimationManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationManager : MonoBehaviour
+{
+    Animator anim;
+    int moveHash = Animator.StringToHash("Move");
+    int idleHash = Animator.StringToHash("Idle");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Play Walk Animation
+        if (BoardManager.instance.IsPlayerMoving)
+            anim.SetTrigger(moveHash);
+        else
+            anim.SetTrigger(idleHash);
+    }
+}
