a27996615386fa483981e11dfc692b80335805da
Hudhaifa-PC\Hudhaifa <info@huthaifa-abd.com>
2019-01-26 15:56:53 +0100

    [Code:Art] Change Cursor On Player Move

---
+++ Assets/dreimeilenstiefel/Scripts/Board and Grid/CursorManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Change Cursor to footsteps
+        if(BoardManager.instance.IsPlayerMoving)
+            Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        else
+            Cursor.SetCursor(null, Vector2.zero, cursorMode);
+    }
+}
